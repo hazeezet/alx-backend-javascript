196a1eec036d82b1e1fc43df9cc1794a7f14eacc
hazeezet <hazeezet@gmail.com>
2023-09-28 03:28:35 +0100

    fix: lint

--- 0x00-ES6_basic/.eslintrc.js
+++ 0x00-ES6_basic/.eslintrc.js
@@ -20,6 +20,7 @@ module.exports = {
     rules: {
       'no-console': 'off',
       'no-shadow': 'off',
+      'indent': ['error', 4],
       'no-restricted-syntax': [
         'error',
         'LabeledStatement',
